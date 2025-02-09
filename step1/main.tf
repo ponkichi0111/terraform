637e195843b08f0bd33d14325b3dfaa9c5e75fb0
財津宏幸 <zaitsuhiroyuki@zaitsuhiroyukinoMacBook-Air.local>
2025-02-09 14:39:15 +0900

    github actions try

--- step1/main.tf
+++ step1/main.tf
@@ -10,70 +10,70 @@ resource "aws_vpc" "example_vpc" {
   }
 }
 
-resource "aws_subnet" "example_pub1" {
-  vpc_id = aws_vpc.example_vpc.id
-  cidr_block = "10.0.0.0/20"
-  map_public_ip_on_launch = true
+# resource "aws_subnet" "example_pub1" {
+#   vpc_id = aws_vpc.example_vpc.id
+#   cidr_block = "10.0.0.0/20"
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "terraform-example"
-  }
-}
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
 
-resource "aws_internet_gateway" "example_ig" {
-  vpc_id = aws_vpc.example_vpc.id
+# resource "aws_internet_gateway" "example_ig" {
+#   vpc_id = aws_vpc.example_vpc.id
 
-  tags = {
-    Name = "terraform-example"
-  }
-}
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
 
-resource "aws_route_table" "example_rt" {
-  vpc_id = aws_vpc.example_vpc.id
+# resource "aws_route_table" "example_rt" {
+#   vpc_id = aws_vpc.example_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example_ig.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.example_ig.id
+#   }
 
-  tags = {
-    Name = "terraform-example"
-  }
-}
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
 
-resource "aws_route_table_association" "example_rt_a" {
-  subnet_id = aws_subnet.example_pub1.id
-  route_table_id = aws_route_table.example_rt.id
-}
+# resource "aws_route_table_association" "example_rt_a" {
+#   subnet_id = aws_subnet.example_pub1.id
+#   route_table_id = aws_route_table.example_rt.id
+# }
 
-resource "aws_instance" "example_instance" {
-  ami = "ami-0a290015b99140cd1"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.example_pub1.id
-  associate_public_ip_address = true
-  vpc_security_group_ids = [ aws_security_group.example_sg.id ]
+# resource "aws_instance" "example_instance" {
+#   ami = "ami-0a290015b99140cd1"
+#   instance_type = "t2.micro"
+#   subnet_id = aws_subnet.example_pub1.id
+#   associate_public_ip_address = true
+#   vpc_security_group_ids = [ aws_security_group.example_sg.id ]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
 
-  user_data_replace_on_change = true
+#   user_data_replace_on_change = true
 
-  tags = {
-    Name ="terraform-example"
-  }
-}
+#   tags = {
+#     Name ="terraform-example"
+#   }
+# }
 
-resource "aws_security_group" "example_sg" {
-  name = "terraform-example-sg"
-  vpc_id = aws_vpc.example_vpc.id
+# resource "aws_security_group" "example_sg" {
+#   name = "terraform-example-sg"
+#   vpc_id = aws_vpc.example_vpc.id
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
