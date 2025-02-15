fd60ba627f4b1efa7fa6fda13a569c98c4e47cb1
財津宏幸 <zaitsuhiroyuki@zaitsuhiroyukinoMacBook-Air.local>
2025-02-15 14:24:54 +0900

    Directory structure change

---
+++ stage/service/webserver-cluster/main.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP request"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_subnet" "example_pub1" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/20"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_internet_gateway" "example_ig" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_route_table" "example_rt" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_ig.id
+  }
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_route_table_association" "example_rt_a" {
+  subnet_id = aws_subnet.example_pub1.id
+  route_table_id = aws_route_table.example_rt.id
+}
+
+resource "aws_instance" "example_instance" {
+  ami = "ami-0a290015b99140cd1"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_pub1.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [ aws_security_group.example_sg.id ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name ="terraform-example"
+  }
+}
+
+resource "aws_security_group" "example_sg" {
+  name = "terraform-example-sg"
+  vpc_id = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example_instance.public_ip
+  description = "The public IP address of the web server"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-hiroyuki"
+    key    = "stage/service/webserver-cluster/terraform.tfstate"
+    region = "ap-northeast-1"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
